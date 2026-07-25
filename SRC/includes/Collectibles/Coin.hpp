d1f4a57c0c03257a1c1192603d90464dd47d7da2
CowboyDreamVI64 <140860073+CowboyDreamVI64@users.noreply.github.com>
2026-07-25 15:45:39 -0400

    Add files via upload

---
+++ SRC/includes/Collectibles/Coin.hpp
@@ -0,0 +1,29 @@
+#pragma once
+#include "Collectible.hpp"
+#include "Player.hpp"
+
+class Coin : public Collectible
+{
+	int value;
+
+	Coin(const Vector2& pos, int value = 1)
+		: Collectible(pos), value(value) {}
+
+	// What happens when the player touches the coin
+	void applyEffect(Player& player) override
+	{
+		player.coins += value;
+		collected = true;
+	}
+
+	// Draw only if not collected
+	void draw(ViewPort& camera) override
+	{
+		if (!collected)
+		{
+			// Replace this with sprite system
+			// An example would be like sprites.add("coin", position, camera);
+		}
+	}
+
+};
